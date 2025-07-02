cb366646ab4c2c156cf17270c42478192090ff6e
anilkumar <anil264kumar5@gmail.com>
2025-07-02 17:50:13 +0530

    changed cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const dotenv = require('dotenv')
 const express = require("express");
+const cors = require('cors')
 const app = express();
+
 
 dotenv.config()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/interest", (req, res) => {
   const principal = parseFloat(req.query.p);
